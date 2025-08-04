843fe467c7853242752c1202aade6539f08f83c8
AmyCur <AmyCurry002@proton.me>
2025-08-04 19:53:11 +0100

    Fixed typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-  site: 'https://amycur.github.io',
+  site: 'https://AmyCur.github.io',
 })
