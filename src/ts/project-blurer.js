b86ddb8855f2c18e132360be4da8ed64662a774c
AmyCur <AmyCurry002@proton.me>
2025-09-29 22:36:51 +0100

    When will i be good enough :(

--- src/ts/project-blurer.js
+++ src/ts/project-blurer.js
@@ -1,18 +1,31 @@
-let selected;
+// let selected;
 
-addEventListener("DOMContentLoaded", () => {
-    const projects = document.getElementsByClassName("project");
-    projects[0].style.borderRadius = "1em 0 0 1em";
-    projects[projects.length-1].style.borderRadius = "0 1em 1em 0";
+// addEventListener("DOMContentLoaded", () => {
+//     const projects = document.getElementsByClassName("project");
+//     const holders = document.getElementsByClassName("background");
+    
+//     // holders[0].style.borderRadius = "1em 0 0 1em";
+//     // holders[holders.length-1].style.borderRadius = "0 1em 1em 0";
 
-    for (const p of projects) {
-        p.addEventListener("mouseover", (p) =>{
-            selected = p;
-            console.log(selected);
+//     // projects[0].style.borderRadius = "1em 0 0 1em";
+//     // projects[projects.length-1].style.borderRadius = "0 1em 1em 0";
 
-            for(const t of projects){
-                if(t !== p) t.style.backgroundColor = "black";
-            }
-        });
-    }
-});
+//     for (const p of projects) {
+//         p.addEventListener("mouseover", () =>{
+ 
+//             console.log(p);
+
+//             for(const t of projects){
+//                 // if(t !== p) t.style.backdropFilter = "blur(.3em)";
+
+//             }
+//         });
+
+//         p.addEventListener("mouseout", () =>{
+//             for(const t of projects){
+//                 if(t !== p) t.style.backdropFilter = "blur(0em)";
+
+//             }
+//         });
+//     }
+// });
