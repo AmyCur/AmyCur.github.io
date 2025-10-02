8768e625abb4e574970fa0aa22c552b79a6207c6
Dylan.Curry (13A) <Dylan.Curry@utcmediacityuk.org.uk>
2025-10-02 13:26:09 +0100

    Restarted

--- src/ts/header-remover.ts
+++
@@ -1,9 +0,0 @@
-addEventListener("DOMContentLoaded", () => {
-    const projects = document.getElementById("projects");
-
-    for(const child of projects!.children){
-        if(child.querySelector(".cards")!.childElementCount < 1) child.remove();
-        else console.log(child);
-    }
-
-});
